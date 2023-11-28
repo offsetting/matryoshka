ee5de1ebdc8b35d7c7441138e7a8f355e365f4a7
Marcel <me@m4rc3l.de>
2023-11-28 19:15:59 +0100

    formatting

--- matryoshka-cli/src/main.rs
+++ matryoshka-cli/src/main.rs
@@ -10,106 +10,106 @@ use matryoshka_core::{decode, encode};
 #[derive(Parser)]
 #[clap(version)]
 struct Args {
-    #[clap(subcommand)]
-    command: Command,
+  #[clap(subcommand)]
+  command: Command,
 }
 
 #[derive(Clone, ValueEnum)]
 pub enum Endian {
-    Big,
-    Little,
+  Big,
+  Little,
 }
 
 #[derive(Clone, ValueEnum)]
 pub enum Format {
-    Json,
-    Yaml,
+  Json,
+  Yaml,
 }
 
 impl Display for Endian {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Endian::Big => f.write_str("big"),
-            Endian::Little => f.write_str("little"),
-        }
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    match self {
+      Endian::Big => f.write_str("big"),
+      Endian::Little => f.write_str("little"),
     }
+  }
 }
 
 impl Display for Format {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Format::Json => f.write_str("json"),
-            Format::Yaml => f.write_str("yaml"),
-        }
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    match self {
+      Format::Json => f.write_str("json"),
+      Format::Yaml => f.write_str("yaml"),
     }
+  }
 }
 
 impl From<Endian> for matryoshka_core::Endian {
-    fn from(value: Endian) -> Self {
-        match value {
-            Endian::Big => Self::Big,
-            Endian::Little => Self::Little,
-        }
+  fn from(value: Endian) -> Self {
+    match value {
+      Endian::Big => Self::Big,
+      Endian::Little => Self::Little,
     }
+  }
 }
 
 #[derive(Subcommand)]
 enum Command {
-    Decode {
-        in_file: PathBuf,
-        out_file: PathBuf,
-        #[clap(short, long, default_value_t = Endian::Little)]
-        endian: Endian,
-        #[clap(short, long, default_value_t = Format::Json)]
-        format: Format,
-    },
-    Encode {
-        in_file: PathBuf,
-        out_file: PathBuf,
-        #[clap(short, long, default_value_t = Endian::Little)]
-        endian: Endian,
-        #[clap(short, long, default_value_t = Format::Json)]
-        format: Format,
-    },
+  Decode {
+    in_file: PathBuf,
+    out_file: PathBuf,
+    #[clap(short, long, default_value_t = Endian::Little)]
+    endian: Endian,
+    #[clap(short, long, default_value_t = Format::Json)]
+    format: Format,
+  },
+  Encode {
+    in_file: PathBuf,
+    out_file: PathBuf,
+    #[clap(short, long, default_value_t = Endian::Little)]
+    endian: Endian,
+    #[clap(short, long, default_value_t = Format::Json)]
+    format: Format,
+  },
 }
 
 fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
+  let args = Args::parse();
 
-    match args.command {
-        Command::Decode {
-            in_file,
-            out_file,
-            endian,
-            format,
-        } => {
-            let mut file = BufReader::new(File::open(in_file)?);
+  match args.command {
+    Command::Decode {
+      in_file,
+      out_file,
+      endian,
+      format,
+    } => {
+      let mut file = BufReader::new(File::open(in_file)?);
 
-            let data = decode(&mut file, endian.into())?;
+      let data = decode(&mut file, endian.into())?;
 
-            let mut file = BufWriter::new(File::create(out_file)?);
+      let mut file = BufWriter::new(File::create(out_file)?);
 
-            match format {
-                Format::Json => serde_json::to_writer_pretty(&mut file, &data)?,
-                Format::Yaml => serde_yaml::to_writer(&mut file, &data)?,
-            }
-        }
-        Command::Encode {
-            in_file,
-            out_file,
-            endian,
-            format,
-        } => {
-            let file = BufReader::new(File::open(in_file)?);
-            let data = match format {
-                Format::Json => serde_json::from_reader(file)?,
-                Format::Yaml => serde_yaml::from_reader(file)?,
-            };
-
-            let mut file = BufWriter::new(File::create(out_file)?);
-            encode(&mut file, data, endian.into())?;
-        }
+      match format {
+        Format::Json => serde_json::to_writer_pretty(&mut file, &data)?,
+        Format::Yaml => serde_yaml::to_writer(&mut file, &data)?,
+      }
     }
+    Command::Encode {
+      in_file,
+      out_file,
+      endian,
+      format,
+    } => {
+      let file = BufReader::new(File::open(in_file)?);
+      let data = match format {
+        Format::Json => serde_json::from_reader(file)?,
+        Format::Yaml => serde_yaml::from_reader(file)?,
+      };
 
-    Ok(())
+      let mut file = BufWriter::new(File::create(out_file)?);
+      encode(&mut file, data, endian.into())?;
+    }
+  }
+
+  Ok(())
 }
--- matryoshka-core/src/lib.rs
+++ matryoshka-core/src/lib.rs
@@ -15,291 +15,291 @@ mod node;
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(untagged)]
 pub enum ContainerData {
-    Single(Data),
-    Multiple(Vec<Data>),
+  Single(Data),
+  Multiple(Vec<Data>),
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(untagged)]
 pub enum Data {
-    Container(IndexMap<String, ContainerData>),
+  Container(IndexMap<String, ContainerData>),
 
-    Binary(#[serde(with = "base64")] Vec<u8>),
+  Binary(#[serde(with = "base64")] Vec<u8>),
 
-    Int(i32),
-    IntVec(Vec<i32>),
+  Int(i32),
+  IntVec(Vec<i32>),
 
-    Float(f32),
-    FloatVec(Vec<f32>),
+  Float(f32),
+  FloatVec(Vec<f32>),
 
-    String(String),
-    StringVec(Vec<String>),
+  String(String),
+  StringVec(Vec<String>),
 }
 
 mod base64 {
-    use base64::{engine::general_purpose, Engine as _};
-    use serde::{Deserialize, Serialize};
-    use serde::{Deserializer, Serializer};
+  use base64::{engine::general_purpose, Engine as _};
+  use serde::{Deserialize, Serialize};
+  use serde::{Deserializer, Serializer};
 
-    pub fn serialize<S: Serializer>(v: &Vec<u8>, s: S) -> Result<S::Ok, S::Error> {
-        let base64 = general_purpose::STANDARD_NO_PAD.encode(v);
-        String::serialize(&format!("base64:{base64}"), s)
-    }
+  pub fn serialize<S: Serializer>(v: &Vec<u8>, s: S) -> Result<S::Ok, S::Error> {
+    let base64 = general_purpose::STANDARD_NO_PAD.encode(v);
+    String::serialize(&format!("base64:{base64}"), s)
+  }
 
-    pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<u8>, D::Error> {
-        let base64 = String::deserialize(d)?;
-        match base64.strip_prefix("base64:") {
-            None => Err(serde::de::Error::custom("missing \"base64:\" prefix")),
-            Some(base64) => general_purpose::STANDARD_NO_PAD
-                .decode(base64.as_bytes())
-                .map_err(serde::de::Error::custom),
-        }
+  pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<u8>, D::Error> {
+    let base64 = String::deserialize(d)?;
+    match base64.strip_prefix("base64:") {
+      None => Err(serde::de::Error::custom("missing \"base64:\" prefix")),
+      Some(base64) => general_purpose::STANDARD_NO_PAD
+        .decode(base64.as_bytes())
+        .map_err(serde::de::Error::custom),
     }
+  }
 }
 
 pub fn decode<R: Read + Seek>(
-    read: &mut R,
-    endian: Endian,
+  read: &mut R,
+  endian: Endian,
 ) -> anyhow::Result<IndexMap<String, ContainerData>> {
-    let magic: [u8; 8] = read.read_type(endian)?;
+  let magic: [u8; 8] = read.read_type(endian)?;
 
-    if &magic
-        != match endian {
-            Endian::Big => b"\x45\x01\x76\x29\x3f\x8c\xcc\xcd",
-            Endian::Little => b"\x29\x76\x01\x45\xCD\xCC\x8C\x3F",
-        }
-    {
-        return Err(anyhow!("Invalid magic, maybe wrong endian?"));
+  if &magic
+    != match endian {
+      Endian::Big => b"\x45\x01\x76\x29\x3f\x8c\xcc\xcd",
+      Endian::Little => b"\x29\x76\x01\x45\xCD\xCC\x8C\x3F",
     }
+  {
+    return Err(anyhow!("Invalid magic, maybe wrong endian?"));
+  }
 
-    let header: OctHeader = read.read_type(endian)?;
+  let header: OctHeader = read.read_type(endian)?;
 
-    // 40 byte padding
-    read.seek(SeekFrom::Current(40))?;
+  // 40 byte padding
+  read.seek(SeekFrom::Current(40))?;
 
-    let start = read.stream_position()?;
-    let mut string_table = Vec::new();
-    while (read.stream_position()? - start) < header.string_table_size as u64 {
-        let null_string: NullString = read.read_type(endian)?;
-        string_table.push(null_string.to_string());
-    }
+  let start = read.stream_position()?;
+  let mut string_table = Vec::new();
+  while (read.stream_position()? - start) < header.string_table_size as u64 {
+    let null_string: NullString = read.read_type(endian)?;
+    string_table.push(null_string.to_string());
+  }
 
-    let start = read.stream_position()?;
+  let start = read.stream_position()?;
 
+  let RawNode { level, node } = read.read_type_args(endian, string_table.as_slice())?;
+
+  let root_level = level;
+  let mut root_node = node;
+
+  while (read.stream_position()? - start) < header.data_tree_size as u64 {
     let RawNode { level, node } = read.read_type_args(endian, string_table.as_slice())?;
 
-    let root_level = level;
-    let mut root_node = node;
+    let mut curr_level = root_level;
+    let mut curr_node = &mut root_node;
 
-    while (read.stream_position()? - start) < header.data_tree_size as u64 {
-        let RawNode { level, node } = read.read_type_args(endian, string_table.as_slice())?;
+    while curr_level < level {
+      curr_level += 1;
+      let nodes = if let NodeData::Container(children) = &mut curr_node.data {
+        children
+      } else {
+        return Err(anyhow!("Expected container"));
+      };
 
-        let mut curr_level = root_level;
-        let mut curr_node = &mut root_node;
-
-        while curr_level < level {
-            curr_level += 1;
-            let nodes = if let NodeData::Container(children) = &mut curr_node.data {
-                children
-            } else {
-                return Err(anyhow!("Expected container"));
-            };
-
-            if curr_level == level {
-                nodes.push(node);
-                break;
-            } else {
-                curr_node = nodes.last_mut().unwrap();
-            }
-        }
+      if curr_level == level {
+        nodes.push(node);
+        break;
+      } else {
+        curr_node = nodes.last_mut().unwrap();
+      }
     }
+  }
 
-    if let Data::Container(children) = root_node.data.try_into()? {
-        Ok(children)
-    } else {
-        Err(anyhow!("Expected root node to be an container"))
-    }
+  if let Data::Container(children) = root_node.data.try_into()? {
+    Ok(children)
+  } else {
+    Err(anyhow!("Expected root node to be an container"))
+  }
 }
 
 pub fn encode<R: Write + Seek>(
-    write: &mut R,
-    data: IndexMap<String, ContainerData>,
-    endian: Endian,
+  write: &mut R,
+  data: IndexMap<String, ContainerData>,
+  endian: Endian,
 ) -> anyhow::Result<()> {
-    let mut nodes = Vec::new();
-    nodes.push(RawNode {
-        level: 0,
-        node: Node {
-            id: "".to_string(),
-            data: NodeData::Container(vec![]),
-        },
-    });
-    extract_nodes(&mut nodes, data, 1);
+  let mut nodes = Vec::new();
+  nodes.push(RawNode {
+    level: 0,
+    node: Node {
+      id: "".to_string(),
+      data: NodeData::Container(vec![]),
+    },
+  });
+  extract_nodes(&mut nodes, data, 1);
 
-    let mut strings = IndexSet::new();
-    for RawNode { node, .. } in &nodes {
-        if let Some((key, name)) = node.id.split_once('#') {
-            strings.insert(key.to_string());
-            strings.insert(name.to_string());
-        } else {
-            strings.insert(node.id.to_string());
+  let mut strings = IndexSet::new();
+  for RawNode { node, .. } in &nodes {
+    if let Some((key, name)) = node.id.split_once('#') {
+      strings.insert(key.to_string());
+      strings.insert(name.to_string());
+    } else {
+      strings.insert(node.id.to_string());
+    }
+
+    match &node.data {
+      NodeData::String(data) => {
+        strings.insert(data.to_string());
+      }
+      NodeData::StringVec(data) => {
+        for x in data {
+          strings.insert(x.to_string());
         }
+      }
 
-        match &node.data {
-            NodeData::String(data) => {
-                strings.insert(data.to_string());
-            }
-            NodeData::StringVec(data) => {
-                for x in data {
-                    strings.insert(x.to_string());
-                }
-            }
-
-            // TODO: binary file name hint
-            NodeData::Binary(_) => {}
-            _ => {}
-        }
+      // TODO: binary file name hint
+      NodeData::Binary(_) => {}
+      _ => {}
     }
+  }
 
-    let strings = strings.into_iter().collect::<Vec<_>>();
+  let strings = strings.into_iter().collect::<Vec<_>>();
 
-    write.write_type(
-        match endian {
-            Endian::Big => b"\x45\x01\x76\x29\x3f\x8c\xcc\xcd",
-            Endian::Little => b"\x29\x76\x01\x45\xCD\xCC\x8C\x3F",
-        },
-        endian,
-    )?;
+  write.write_type(
+    match endian {
+      Endian::Big => b"\x45\x01\x76\x29\x3f\x8c\xcc\xcd",
+      Endian::Little => b"\x29\x76\x01\x45\xCD\xCC\x8C\x3F",
+    },
+    endian,
+  )?;
 
-    // 12 byte header + 40 byte padding
-    write.seek(SeekFrom::Current(12 + 40))?;
+  // 12 byte header + 40 byte padding
+  write.seek(SeekFrom::Current(12 + 40))?;
 
-    let start = write.stream_position()?;
-    for x in &strings {
-        write.write_type(&NullString::from(x.as_str()), endian)?;
-    }
-    let string_size = write.stream_position()? - start;
+  let start = write.stream_position()?;
+  for x in &strings {
+    write.write_type(&NullString::from(x.as_str()), endian)?;
+  }
+  let string_size = write.stream_position()? - start;
 
-    let start = write.stream_position()?;
-    for x in &nodes {
-        write.write_type_args(x, endian, strings.as_slice())?;
-    }
-    let node_size = write.stream_position()? - start;
+  let start = write.stream_position()?;
+  for x in &nodes {
+    write.write_type_args(x, endian, strings.as_slice())?;
+  }
+  let node_size = write.stream_position()? - start;
 
-    write.seek(SeekFrom::Start(8))?;
-    write.write_type(
-        &OctHeader {
-            string_table_size: string_size as u32,
-            data_tree_size: node_size as u32,
-        },
-        endian,
-    )?;
+  write.seek(SeekFrom::Start(8))?;
+  write.write_type(
+    &OctHeader {
+      string_table_size: string_size as u32,
+      data_tree_size: node_size as u32,
+    },
+    endian,
+  )?;
 
-    Ok(())
+  Ok(())
 }
 
 fn extract_nodes(nodes: &mut Vec<RawNode>, data: IndexMap<String, ContainerData>, level: u8) {
-    for (id, node) in data {
-        let n = match node {
-            ContainerData::Single(x) => vec![x],
-            ContainerData::Multiple(x) => x,
-        };
+  for (id, node) in data {
+    let n = match node {
+      ContainerData::Single(x) => vec![x],
+      ContainerData::Multiple(x) => x,
+    };
 
-        for node in n {
-            if let Data::Container(childs) = node {
-                nodes.push(RawNode {
-                    level,
-                    node: Node {
-                        id: id.clone(),
-                        data: NodeData::Container(vec![]),
-                    },
-                });
-                extract_nodes(nodes, childs, level + 1);
-            } else {
-                nodes.push(RawNode {
-                    level,
-                    node: Node {
-                        id: id.clone(),
-                        data: match node {
-                            Data::Container(..) => unreachable!(),
-                            Data::String(data) => NodeData::String(data),
-                            Data::StringVec(data) => NodeData::StringVec(data),
-                            Data::Float(data) => NodeData::Float(data),
-                            Data::FloatVec(data) => NodeData::FloatVec(data),
-                            Data::Int(data) => NodeData::Int(data),
-                            Data::IntVec(data) => NodeData::IntVec(data),
-                            Data::Binary(data) => NodeData::Binary(data),
-                        },
-                    },
-                });
-            }
-        }
+    for node in n {
+      if let Data::Container(childs) = node {
+        nodes.push(RawNode {
+          level,
+          node: Node {
+            id: id.clone(),
+            data: NodeData::Container(vec![]),
+          },
+        });
+        extract_nodes(nodes, childs, level + 1);
+      } else {
+        nodes.push(RawNode {
+          level,
+          node: Node {
+            id: id.clone(),
+            data: match node {
+              Data::Container(..) => unreachable!(),
+              Data::String(data) => NodeData::String(data),
+              Data::StringVec(data) => NodeData::StringVec(data),
+              Data::Float(data) => NodeData::Float(data),
+              Data::FloatVec(data) => NodeData::FloatVec(data),
+              Data::Int(data) => NodeData::Int(data),
+              Data::IntVec(data) => NodeData::IntVec(data),
+              Data::Binary(data) => NodeData::Binary(data),
+            },
+          },
+        });
+      }
     }
+  }
 }
 
 impl TryFrom<NodeData> for Data {
-    type Error = anyhow::Error;
+  type Error = anyhow::Error;
 
-    fn try_from(node_data: NodeData) -> Result<Self, Self::Error> {
-        Ok(match node_data {
-            NodeData::Container(child) => {
-                let mut childs = IndexMap::new();
-                for node in child {
-                    if childs.contains_key(&node.id) {
-                        let data = childs.remove(&node.id).unwrap();
-                        match data {
-                            ContainerData::Single(first) => childs.insert(
-                                node.id,
-                                ContainerData::Multiple(vec![first, node.data.try_into()?]),
-                            ),
-                            ContainerData::Multiple(mut list) => {
-                                list.push(node.data.try_into()?);
-                                childs.insert(node.id, ContainerData::Multiple(list))
-                            }
-                        };
-                    } else {
-                        childs.insert(node.id, ContainerData::Single(node.data.try_into()?));
-                    }
-                }
-                Data::Container(childs)
-            }
-            NodeData::String(str) => Data::String(str),
-            NodeData::StringVec(str_vec) => Data::StringVec(str_vec),
-            NodeData::Float(str_vec) => Data::Float(str_vec),
-            NodeData::FloatVec(str_vec) => Data::FloatVec(str_vec),
-            NodeData::Int(str_vec) => Data::Int(str_vec),
-            NodeData::IntVec(str_vec) => Data::IntVec(str_vec),
-            NodeData::Binary(str_vec) => Data::Binary(str_vec),
-        })
-    }
+  fn try_from(node_data: NodeData) -> Result<Self, Self::Error> {
+    Ok(match node_data {
+      NodeData::Container(child) => {
+        let mut childs = IndexMap::new();
+        for node in child {
+          if childs.contains_key(&node.id) {
+            let data = childs.remove(&node.id).unwrap();
+            match data {
+              ContainerData::Single(first) => childs.insert(
+                node.id,
+                ContainerData::Multiple(vec![first, node.data.try_into()?]),
+              ),
+              ContainerData::Multiple(mut list) => {
+                list.push(node.data.try_into()?);
+                childs.insert(node.id, ContainerData::Multiple(list))
+              }
+            };
+          } else {
+            childs.insert(node.id, ContainerData::Single(node.data.try_into()?));
+          }
+        }
+        Data::Container(childs)
+      }
+      NodeData::String(str) => Data::String(str),
+      NodeData::StringVec(str_vec) => Data::StringVec(str_vec),
+      NodeData::Float(str_vec) => Data::Float(str_vec),
+      NodeData::FloatVec(str_vec) => Data::FloatVec(str_vec),
+      NodeData::Int(str_vec) => Data::Int(str_vec),
+      NodeData::IntVec(str_vec) => Data::IntVec(str_vec),
+      NodeData::Binary(str_vec) => Data::Binary(str_vec),
+    })
+  }
 }
 
 impl From<Data> for NodeData {
-    fn from(value: Data) -> Self {
-        match value {
-            Data::Container(child) => {
-                let mut childs = Vec::with_capacity(child.len());
-                for (id, data) in child {
-                    let n = match data {
-                        ContainerData::Single(x) => vec![x],
-                        ContainerData::Multiple(x) => x,
-                    };
-                    for data in n {
-                        childs.push(Node {
-                            id: id.clone(),
-                            data: data.into(),
-                        });
-                    }
-                }
-                NodeData::Container(childs)
-            }
-            Data::String(data) => NodeData::String(data),
-            Data::StringVec(data) => NodeData::StringVec(data),
-            Data::Float(data) => NodeData::Float(data),
-            Data::FloatVec(data) => NodeData::FloatVec(data),
-            Data::Int(data) => NodeData::Int(data),
-            Data::IntVec(data) => NodeData::IntVec(data),
-            Data::Binary(data) => NodeData::Binary(data),
+  fn from(value: Data) -> Self {
+    match value {
+      Data::Container(child) => {
+        let mut childs = Vec::with_capacity(child.len());
+        for (id, data) in child {
+          let n = match data {
+            ContainerData::Single(x) => vec![x],
+            ContainerData::Multiple(x) => x,
+          };
+          for data in n {
+            childs.push(Node {
+              id: id.clone(),
+              data: data.into(),
+            });
+          }
         }
+        NodeData::Container(childs)
+      }
+      Data::String(data) => NodeData::String(data),
+      Data::StringVec(data) => NodeData::StringVec(data),
+      Data::Float(data) => NodeData::Float(data),
+      Data::FloatVec(data) => NodeData::FloatVec(data),
+      Data::Int(data) => NodeData::Int(data),
+      Data::IntVec(data) => NodeData::IntVec(data),
+      Data::Binary(data) => NodeData::Binary(data),
     }
+  }
 }
